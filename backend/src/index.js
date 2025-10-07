6222795f2ee7a4ad5306348ccdb11dadb58737f7
Copojaab <palermijacopo@gmail.com>
2025-10-07 11:53:01 +0200

    feat: aggiunta la traduzione da json a stringa e l'aggiunta della matrice A al file di testo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,48 @@
-// importa la lib express
+// librerie
 const express = require('express');
+const fs = require('fs'); //file system
+const path = require('path'); //gestione percorsi cartelle
+// const exec = require('exec'); //comandi terminale
+const crypto = require('crypto'); //id unici random
+
 // crea l'app server
 const app = express();
-// scelta porta
+app.use(express.json()); //middleware 
+
+
 const PORT = 3000;
 
 // ---- DEFINIZIONE ROTTE ---- 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //test server
     res.send('Server funzionante');
+});
+
+// nuovo job
+app.post('/api/jobs', (req, res) =>{
+    //a. creazione id unico
+    const jobID = crypto.randomBytes(8).toString('hex');
+    console.log(`[${jobID}] Richiesta di calcolo ricevuta`);
+
+    //b. prendo i percorsi dei file
+    const workerDir = path.join(__dirname, '..', '..', 'worker');
+    const fileA = path.join(workerDir, `matrice_a_${jobID}.txt`);
+    const fileB = path.join(workerDir, `matrice_b_${jobID}.txt`);
+    const fileC = path.join(workerDir, `risultato_c_${jobID}.txt`);
+
+    //c. conversione matrici da json a stringhe
+    const matrixToString = (matrix) => matrix.map(row => row.join(' ')).join('\n');
+    const dataA = matrixToString(req.body.matrixA);
+    const dataB = matrixToString(req.body.matrixB);
+
+    // d scrivo il primo file
+    fs.writeFile(fileA, dataA, (err) =>{
+        if(err){
+            console.error(`[${jobID}] Errore scrivendo file A: `, err);
+            return res.status(500).json({error : 'Errore interno del server' })
+        }
+        console.log(`[${jobID}] File ${fileA} creato.`);
+        res.json({ message: 'File A scritto con successo', jobID: jobID});
+    })
 });
 
 // ---- AVVIO DEL SERVER ----
